3122a257481da30c5bd64d5e63b5eb0ea4f6d13e
sreedhar-gif <71756537+sreedhar-gif@users.noreply.github.com>
2022-07-27 15:47:15 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿string[] name = { "akshay", "shreedhar", "pramod", "surya", "hari", "goutham", "simran", "arshit" };
+foreach (string var in name)
+{
+    if (var.Contains("s") == true)
+    {
+        Console.WriteLine(var);
+    }
+}
+
+int minLength = name.Min(y => y.Length);
+var shortest = name.FirstOrDefault(x => x.Length == minLength);
+Console.WriteLine(shortest);
+
+string[] words1 = { "believe", "relief", "receipt", "field" };
+foreach (string word in words1)
+{
+    Console.WriteLine(word + "--" + word.Length);
+}
+
+foreach (string var in words1)
+{
+    if (var.Contains("ie") == true)
+    {
+        Console.WriteLine(var);
+    }
+}
+var result = name.Reverse();
+Console.WriteLine(result);
+foreach (var val in result)
+{
+    Console.WriteLine(val);
+}
+
+for (int i = 0; i < name.Length; i++)
+{
+    if(i%2 ==0)
+    {
+        Console.WriteLine(name[i]);
+    }
+}
